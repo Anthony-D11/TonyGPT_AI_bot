110c172cbd6252a4c3f56e385e9126fb08d4186b
Anthony Nguyen <48093087+Anthony-D11@users.noreply.github.com>
2023-04-07 23:33:39 -0300

    Get the bot respond to a single message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Configuration, OpenAIApi, CreateCompletionRequest } from 'openai';
+import { config } from 'dotenv';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public botResponse: string = "Bot response goes HERE";
+  public promptText: string = "";
+  
+  constructor() {}
+
+
+  
   title = 'TonyGPT_AI_bot';
+
+  async sendMessage() {
+    try {
+      let configuration = new Configuration({
+        //apiKey: process.env['OPENAI_API_KEY'],
+        apiKey: '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'
+      });
+      let openai = new OpenAIApi(configuration);
+
+      let requestData: CreateCompletionRequest = {
+        model: 'text-davinci-003',
+        prompt: this.promptText,
+        temperature: 0.95,
+        max_tokens: 150,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      }
+
+
+      let response = await openai.createCompletion(requestData);
+
+      this.botResponse = response.data.choices[0].text!;
+    }catch(error:any) {
+      // Consider adjusting the error handling logic for your use case
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        
+      } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        
+      }
+    }
+    
+  }
 }
