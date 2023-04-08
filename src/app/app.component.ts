8b7dffcf165140d6a8466f89e3b4fe532b816432
Anthony Nguyen <48093087+Anthony-D11@users.noreply.github.com>
2023-04-08 13:34:52 -0300

    Make the bot work for a series of questions and change the model used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Configuration, OpenAIApi, CreateCompletionRequest } from 'openai';
+import { Configuration, OpenAIApi, CreateCompletionRequest, CreateChatCompletionRequest, ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } from 'openai';
 import { config } from 'dotenv';
-
+import { message } from './ultility';
+import { GPT_response_format } from './ultility';
 
 @Component({
   selector: 'app-root',
@@ -9,39 +10,66 @@ import { config } from 'dotenv';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public botResponse: string = "Bot response goes HERE";
   public promptText: string = "";
-  
-  constructor() {}
-
-
+  public showTyping: boolean = false;
+  public messageList: message[] = [];
+  private promptTextList: ChatCompletionRequestMessage[] = [];
+  constructor() {
+    this.pushChat("Chào bạn, mình là TonyGPT. \nMình có thể giúp gì cho bạn hôm nay nhỉ?", true, "bot")
+  }
   
   title = 'TonyGPT_AI_bot';
 
+  pushChat(content: string, bot_response: boolean, cssClass: string) {
+    let singleChat: message = {
+      bot_response: bot_response,
+      content: content,
+      cssClass: cssClass
+    };
+    this.messageList.push(singleChat);
+
+    let sender: ChatCompletionRequestMessageRoleEnum = 'user';
+    if(bot_response) {
+      sender = 'assistant'
+    }
+
+    let singleResponse: ChatCompletionRequestMessage = {
+      role: sender,
+      content: content
+    }
+    this.promptTextList.push(singleResponse);
+  }
+
   async sendMessage() {
     try {
+      const userInputTextArea = document.getElementById('user_input_text') as HTMLTextAreaElement;
+      userInputTextArea.value = ''
+
+      this.pushChat(this.promptText, false, 'user');
       let configuration = new Configuration({
         //apiKey: process.env['OPENAI_API_KEY'],
-        apiKey: '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'
+        apiKey: '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'
       });
       let openai = new OpenAIApi(configuration);
+      
 
-      let requestData: CreateCompletionRequest = {
-        model: 'text-davinci-003',
-        prompt: this.promptText,
-        temperature: 0.95,
-        max_tokens: 150,
+      let requestData: CreateChatCompletionRequest = {
+        model: 'gpt-3.5-turbo', 
+        messages: this.promptTextList,
+        temperature: 0.7,
+        max_tokens: 2500,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
       }
+      this.showTyping = true;
+      let response = await openai.createChatCompletion(requestData);
+      let botResponse = response.data.choices[0].message?.content;
+      this.pushChat(botResponse!, true, 'bot');
+      this.showTyping = false;
 
-
-      let response = await openai.createCompletion(requestData);
-
-      this.botResponse = response.data.choices[0].text!;
     }catch(error:any) {
-      // Consider adjusting the error handling logic for your use case
+      this.showTyping = false;
       if (error.response) {
         console.error(error.response.status, error.response.data);
         
---
+++ src/app/ultility.ts
@@ -0,0 +1,9 @@
+export interface message {
+    bot_response: boolean;
+    content: string;
+    cssClass: string;
+}
+export interface GPT_response_format {
+    role: string;
+    content: string;
+}
